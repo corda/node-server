ad1921b5efab66973f4c2f52c888bd7f298a6ec5
NilsPe <nils.pehoviak@r3.com>
2020-07-21 14:33:56 +0200

    moved default port to application.yml

--- src/main/java/net/corda/explorer/ExplorerServer.java
+++ src/main/java/net/corda/explorer/ExplorerServer.java
@@ -3,14 +3,11 @@ package net.corda.explorer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
-
 @SpringBootApplication
 public class ExplorerServer {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(ExplorerServer.class);
-        app.setDefaultProperties(Collections.singletonMap("server.port", "8580"));
         app.run();
     }
 }
