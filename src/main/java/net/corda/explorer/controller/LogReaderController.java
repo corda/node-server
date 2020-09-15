af4b5e49c9e7f6366f825602b866807a9db16cac
Nikolai Merritt <nikolaimerritt20junk@gmail.com>
2020-09-15 13:06:38 +0100

    Reads files backwards

--- src/main/java/net/corda/explorer/controller/LogReaderController.java
+++ src/main/java/net/corda/explorer/controller/LogReaderController.java
@@ -6,6 +6,7 @@ import net.corda.explorer.model.response.EntriesCountResponse;
 import net.corda.explorer.model.response.LogEntries;
 import net.corda.explorer.model.response.LogEntry;
 import net.corda.explorer.service.StringToEntry;
+import net.corda.explorer.service.impl.ReverseLineInputStream;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,13 +22,12 @@ import java.util.stream.Stream;
 public class LogReaderController {
     @PostMapping("logReader/read")
     public LogEntries getLogEntries(@NotNull @RequestBody ReadRequest readRequest) {
-        final File logFile = new File(pathFromComponents(readRequest.getComponents()));
         final List<LogEntry> entries = new ArrayList<>();
         StringBuilder stringBuilder = new StringBuilder();
         int entriesSeen = 0;
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(logFile))) {
-            for (String line; (line = reader.readLine()) != null && entriesSeen <= readRequest.getStopIndex(); ) {
+        try (BufferedReader backwardsReader = backwardsReaderFromComponents(readRequest.getComponents())) {
+            for (String line; (line = backwardsReader.readLine()) != null && entriesSeen <= readRequest.getStopIndex(); ) {
                 if (isStartOfLog(line)) {
                     if (stringBuilder.length() != 0 && entriesSeen >= readRequest.getStartIndex()) {
                         entries.add(StringToEntry.parse(stringBuilder.toString()));
@@ -40,16 +40,14 @@ public class LogReaderController {
         }
         catch (IOException | ParseException ex) { ex.printStackTrace(); }
 
-        Collections.reverse(entries); // so that the most recent entries are at the top
         return new LogEntries(entries);
     }
 
     @PostMapping("logReader/entriesCount")
     public EntriesCountResponse getEntriesCount(@NotNull @RequestBody EntriesCountRequest request) {
-        final File logFile = new File(pathFromComponents(request.getComponents()));
         int count = 0;
-        try (BufferedReader reader = new BufferedReader(new FileReader(logFile))) {
-            for (String line; (line = reader.readLine()) != null;) {
+        try (BufferedReader backwardsReader = backwardsReaderFromComponents(request.getComponents())) {
+            for (String line; (line = backwardsReader.readLine()) != null;) {
                 if (isStartOfLog(line)) { count++; }
             }
         }
@@ -61,10 +59,12 @@ public class LogReaderController {
         return Stream.of("[INFO ]", "[WARN ]", "[ERROR]").anyMatch(line::startsWith);
     }
 
-    private String pathFromComponents(List<String> components) {
-        return components.stream().reduce(
+    private BufferedReader backwardsReaderFromComponents(List<String> components) throws FileNotFoundException {
+        final String filepath = components.stream().reduce(
                 "", //System.getProperty("user.home"),
                 (acc, next) -> acc + File.separator + next
         );
+        final File file = new File(filepath);
+        return new BufferedReader(new InputStreamReader(new ReverseLineInputStream(file)));
     }
 }
