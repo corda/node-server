4bcbf9eda80d1f0fa511deed3c48db25cf62cf8a
Anthony Nixon <anixon604@gmail.com>
2020-09-22 09:53:42 +0100

    UUID token check added to logreader controller

--- src/main/java/net/corda/explorer/controller/LogReaderController.java
+++ src/main/java/net/corda/explorer/controller/LogReaderController.java
@@ -1,5 +1,6 @@
 package net.corda.explorer.controller;
 
+import net.corda.explorer.exception.AuthenticationException;
 import net.corda.explorer.model.request.EntriesCountRequest;
 import net.corda.explorer.model.request.ReadRequest;
 import net.corda.explorer.model.response.EntriesCountResponse;
@@ -8,6 +9,7 @@ import net.corda.explorer.model.response.LogEntry;
 import net.corda.explorer.service.StringToEntry;
 import net.corda.explorer.service.impl.ReverseLineInputStream;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.*;
@@ -20,8 +22,15 @@ import java.util.stream.Stream;
 @CrossOrigin(origins = "*")
 @RestController
 public class LogReaderController {
+
+    @Value("${servertoken}")
+    private String servertoken;
+
     @PostMapping("logReader/read")
-    public LogEntries getLogEntries(@NotNull @RequestBody ReadRequest readRequest) {
+    public LogEntries getLogEntries(@RequestHeader(value="clienttoken") String clienttoken, @NotNull @RequestBody ReadRequest readRequest) throws AuthenticationException {
+        // auth check
+        if (!servertoken.equals(clienttoken)) throw new AuthenticationException("No valid client token");
+
         final List<LogEntry> entries = new ArrayList<>();
         StringBuilder stringBuilder = new StringBuilder();
         int entriesSeen = 0;
@@ -44,7 +53,10 @@ public class LogReaderController {
     }
 
     @PostMapping("logReader/entriesCount")
-    public EntriesCountResponse getEntriesCount(@NotNull @RequestBody EntriesCountRequest request) {
+    public EntriesCountResponse getEntriesCount(@RequestHeader(value="clienttoken") String clienttoken, @NotNull @RequestBody EntriesCountRequest request) throws AuthenticationException {
+        // auth check
+        if (!servertoken.equals(clienttoken)) throw new AuthenticationException("No valid client token");
+
         int count = 0;
         try (BufferedReader backwardsReader = backwardsReaderFromComponents(request.getComponents())) {
             for (String line; (line = backwardsReader.readLine()) != null;) {
